2242ae322a06159eec22257e89bc2cb8dd8d026b
unknown <diego@rojascr.net>
2020-06-17 08:35:26 -0600

    Teams and player relations

---
+++ SportAcademyManager.Domain/PlayerSkill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportAcademyManager.Domain
+{
+    public class PlayerSkill
+    {
+        public int PlayerId { get; set; }
+        public Player CurrentPlayer { get; set; }
+        public int SkillId { get; set; }
+        public Skill CurrentSkill { get; set; }
+    }
+}
---
+++ SportAcademyManager.Domain/PlayerTeam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportAcademyManager.Domain
+{
+    public class PlayerTeam
+    {
+        public Player CurrentPlayer { get; set; }
+        public int PlayerId { get; set; }
+        public Team CurrentTeam { get; set; }
+        public int TeamId { get; set; }
+    }
+}
